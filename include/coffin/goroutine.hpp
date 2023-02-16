7d4430d2f06ea7861291b83455b4abf1756607d6
root <root@DESKTOP-558O8H2.localdomain>
2023-02-17 02:53:30 +0900

    q

--- include/coffin/goroutine.hpp
+++ include/coffin/goroutine.hpp
@@ -2,7 +2,7 @@
 #include <atomic>
 #include <cassert>
 #include <deque>
-#include <experimental/coroutine>
+#include <coroutine>
 #include <list>
 #include <mutex>
 #include <optional>
@@ -21,10 +21,10 @@ struct ExceptionHandler {
 template <class value_type = void> class Task {
 public:
   struct promise_type;
-  using handle_type = std::experimental::coroutine_handle<promise_type>;
+  using handle_type = std::coroutine_handle<promise_type>;
   struct FinalSuspend {
     bool await_ready() const noexcept { return false; }
-    std::experimental::coroutine_handle<>
+    std::coroutine_handle<>
     await_suspend(handle_type self) noexcept {
       return self.promise().parent;
     }
@@ -33,8 +33,8 @@ public:
   struct Awaiter {
     handle_type self;
     bool await_ready() const noexcept { return false; }
-    std::experimental::coroutine_handle<>
-    await_suspend(std::experimental::coroutine_handle<> h) noexcept {
+    std::coroutine_handle<>
+    await_suspend(std::coroutine_handle<> h) noexcept {
       self.promise().parent = h;
       return self;
     }
@@ -67,10 +67,10 @@ public:
   struct promise_type : promise_type1<value_type> {
     promise_type() {}
     auto get_return_object() { return Task{handle_type::from_promise(*this)}; }
-    std::experimental::suspend_always initial_suspend() { return {}; }
+    std::suspend_always initial_suspend() { return {}; }
     FinalSuspend final_suspend() noexcept { return {}; }
-    std::experimental::coroutine_handle<> parent =
-        std::experimental::noop_coroutine();
+    std::coroutine_handle<> parent =
+        std::noop_coroutine();
   };
   auto operator co_await() { return Awaiter{coro_}; }
 
@@ -91,7 +91,7 @@ private:
 class Goroutine : public std::enable_shared_from_this<Goroutine> {
   friend detail::GoroutineWrapper;
   Task<> task;
-  std::experimental::coroutine_handle<> current_handle;
+  std::coroutine_handle<> current_handle;
   // select
   struct Seldat {
     std::size_t wakeup_id = 0;
@@ -109,14 +109,14 @@ namespace detail {
 struct GoroutineWrapper {
   std::shared_ptr<Goroutine> p;
   Goroutine::Seldat &select() { return p->seldat; }
-  std::experimental::coroutine_handle<> &current_handle() {
+  std::coroutine_handle<> &current_handle() {
     return p->current_handle;
   }
   std::shared_ptr<Goroutine> to_goroutine() { return p; };
 };
 thread_local inline GoroutineWrapper *current_goroutine = nullptr;
 
-GoroutineWrapper ready_park(std::experimental::coroutine_handle<> h) {
+GoroutineWrapper ready_park(std::coroutine_handle<> h) {
   assert(current_goroutine);
   current_goroutine->current_handle() = h;
   return {current_goroutine->to_goroutine()};
@@ -218,7 +218,7 @@ public:
     bool await_ready() const noexcept {
       return false; // 何回もlock取得するわけにいかないので常にawait_suspendで処理する
     }
-    bool await_suspend(std::experimental::coroutine_handle<> h) {
+    bool await_suspend(std::coroutine_handle<> h) {
       std::optional<detail::GoroutineWrapper> next_goroutine;
       bool r = false;
       {
@@ -239,7 +239,7 @@ public:
     // select用
     auto try_nonblock_exec() { return nonblock_send(); }
     void block_exec(std::size_t wakeup_id,
-                    std::experimental::coroutine_handle<> h) {
+                    std::coroutine_handle<> h) {
       it = ch.send_queue.enqueueSudog(
           SendSudog{&val, detail::ready_park(h), wakeup_id, true});
     }
@@ -269,7 +269,7 @@ public:
     bool await_ready() const noexcept {
       return false;
     } // 何回もlock取得するわけにいかないので常にawait_suspendで処理する
-    bool await_suspend(std::experimental::coroutine_handle<> h) {
+    bool await_suspend(std::coroutine_handle<> h) {
       std::optional<detail::GoroutineWrapper> next_goroutine;
       bool r = false;
       {
@@ -288,7 +288,7 @@ public:
 
     auto try_nonblock_exec() { return nonblock_recv(); }
     void block_exec(std::size_t wakeup_id,
-                    std::experimental::coroutine_handle<> h) {
+                    std::coroutine_handle<> h) {
       it = ch.recv_queue.enqueueSudog(
           RecvSudog{&val, detail::ready_park(h), wakeup_id, true});
     }
@@ -367,7 +367,7 @@ struct [[nodiscard]] SelectAwaiter {
   bool await_ready() const noexcept {
     return false; // 何回もlock取得するわけにいかないので常にawait_suspendで処理する
   }
-  bool await_suspend(std::experimental::coroutine_handle<> h) noexcept {
+  bool await_suspend(std::coroutine_handle<> h) noexcept {
     bool r = false;
     std::optional<detail::GoroutineWrapper> next_goroutine;
     { // lock block
@@ -419,6 +419,46 @@ struct [[nodiscard]] SelectAwaiter {
         });
   }
 };
+
+template <class... T>
+auto try_select(T... select_case){
+    std::tuple<T...> sc_list = { select_case... };
+    static constexpr std::size_t N = sizeof...(T);
+    std::size_t wakeup_id = 0;
+    bool r = false;
+    std::optional<detail::GoroutineWrapper> next_goroutine;
+
+    { // lock block
+        auto lock = std::apply(
+            [](auto &... t) { return std::scoped_lock{ t.ch.mutex... }; }, sc_list);
+
+        detail::integral_constant_each(
+            std::make_index_sequence<N>{}, [&](auto I) {
+                if (!r) {
+                    std::tie(r, next_goroutine) =
+                        std::get<I()>(sc_list).try_nonblock_exec();
+                    if (r) {
+                        wakeup_id = I();
+                    }
+                }
+            });
+    } // lock block
+    if (next_goroutine) {
+        detail::integral_constant_each(
+            std::make_index_sequence<N>{}, [&](auto I) {
+                if (I() == wakeup_id) {
+                    std::get<I()>(sc_list).ch.strategy.post_goroutine(
+                        next_goroutine->to_goroutine());
+                }
+            });
+    }
+
+	return detail::integral_constant_map(
+		std::make_index_sequence<N>{}, [&](auto I) {
+			return std::get<I()>(sc_list).select_result(r && I() == wakeup_id);
+		});
+}
+
 
 template <class... T> SelectAwaiter<T...> select(T... select_case) {
   return {{select_case...}};
